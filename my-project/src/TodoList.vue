fca403a5d17e6c667b915dfe6612a6fa5de600dd
daijian <daijian@hrzan.com>
2018-05-22 12:55:53 +0800

    github1

---
+++ my-project/src/TodoList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <input type="text" v-model='inputValue'>
+    <button @click='submit'>提交</button>
+    <todo-item v-for='(item, index) in lists' :key='index' :content='item' :index='index' @delete='deleteFather'></todo-item>
+  </div>
+</template>
+
+<script>
+  import TodoItem from './components/TodoItem.vue'
+
+  export default {
+    data() {
+      return {
+        inputValue: '',
+        lists: []
+      }
+    },
+    methods: {
+      submit() {
+        this.lists.push(this.inputValue);
+      },
+      deleteFather(index) {
+        this.lists.splice(index, 1);
+      }
+    },
+    components: {
+      'todo-item': TodoItem
+    }
+  }
+</script>
+
+<style>
+</style>
---
+++ my-project/src/components/TodoItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <li @click='deletee'>{{content}}</li>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['content', 'index'],
+  methods: {
+    deletee() {
+      this.$emit('delete', this.index);
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
